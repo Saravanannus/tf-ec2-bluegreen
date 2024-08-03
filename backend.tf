2f37518cb7ded4f078552e2bbc572970a3866fa1
Saravanan.B <saravanannus@gmail.com>
2024-08-03 12:31:38 +0800

    7thcommit

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 1.0.0"
   backend "s3" {
-    bucket = "sctp-ce6-tfstate"              # Terraform State bucket name
-    key    = "REPLACE-ec2-bluegreen.tfstate" # Name of your tfstate file
-    region = "ap-southeast-1"                # Terraform State bucket region
+    bucket = "sctp-ce6-tfstate"           # Terraform State bucket name
+    key    = "sara-ec2-bluegreen.tfstate" # Name of your tfstate file
+    region = "ap-southeast-1"             # Terraform State bucket region
   }
 }
 
